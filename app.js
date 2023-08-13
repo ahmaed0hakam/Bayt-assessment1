ea32a26ec19aa35f23ec56ddf07090231cdaa16d
Ahmad Hakam <96107007+ahmaed0hakam@users.noreply.github.com>
2023-08-13 20:55:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-$(document).ready(function() {
-  $('.show-more').on('click', function() {
-    $('footer').toggleClass('expanded');
-    $('.small-cyrcle img').toggleClass('rotate');
-    $('.hidden').toggleClass('visible');
+document.addEventListener("DOMContentLoaded", function() {
+  var showMore = document.querySelector(".show-more");
+  var footer = document.querySelector("footer");
+  var smallCircleImg = document.querySelector(".small-cyrcle img");
+  var hiddenElements = document.querySelectorAll(".hidden");
+
+  showMore.addEventListener("click", function() {
+    footer.classList.toggle("expanded");
+    smallCircleImg.classList.toggle("rotate");
+
+    hiddenElements.forEach(function(hiddenElement) {
+      hiddenElement.classList.toggle("visible");
+    });
   });
 
-  $('.add').on('click', function() {
-    $('.secondary').toggleClass('primary');
-    const $icon = $('.add img');
-    $icon.toggleClass('rotate');
+  var addButton = document.querySelector(".add");
+  var secondaryDiv = document.querySelector(".secondary");
+  var addImg = document.querySelector(".add img");
 
-    if ($icon.attr('src') === 'icons/plus.png') {
-      $icon.attr('src', 'icons/minus.png');
+  addButton.addEventListener("click", function() {
+    secondaryDiv.classList.toggle("primary");
+
+    addImg.classList.toggle("rotate");
+
+    if (addImg.getAttribute("src") === "icons/plus.png") {
+      addImg.setAttribute("src", "icons/minus.png");
     } else {
-      $icon.attr('src', 'icons/plus.png');
+      addImg.setAttribute("src", "icons/plus.png");
     }
   });
 });
 
-function smoothScrollToAnchor(anchorSelector) {
-  document.querySelectorAll(anchorSelector).forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-        window.scrollTo({
-          top: target.offsetTop,
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
-}
 
 function showAlert(message) {
   alert(message);
